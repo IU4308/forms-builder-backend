dde0bb5406e06a9ee6ca5d2942b9c1c4bb66892d
Ilya <ilya671qw@gmail.com>
2025-04-03 19:57:08 +0300

    implement user registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,22 @@
 import { pgTable } from "drizzle-orm/pg-core";
 import { db } from "../config/db.js";
 import { User } from "../models/User.js";
+import bcrypt from 'bcrypt';
 
 
 export const register = async (req, res, next) => {
-    console.log(req.body)
+    const { name, email, password } = req.body 
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = await db.insert(User).values({
+            name,
+            email,
+            password: hashedPassword
+        })
+        res.status(201).json({ message: 'Account has been created' })
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const getUsers = async (req, res) => {
--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -6,5 +6,9 @@ export const errorHandler = (error, req, res, next) => {
         return res.status(400).json(error.issues)
     }
 
+    if (error.code === '23505') {
+        return res.status(409).json({ message: 'A user with this email already exists' })
+    }
+
     return res.status(500).json({ error })
 }
