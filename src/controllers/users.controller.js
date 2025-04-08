df196b4ccd7fde6cd1b4e36c64526f62d8754129
Ilya <ilya671qw@gmail.com>
2025-04-08 13:41:46 +0300

    amend error handling in get user action

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,11 +2,12 @@ import { db } from "../config/db.js";
 import { User } from "../models/User.js";
 import { eq } from "drizzle-orm";
 
-export const getUsers = async (req, res) => {
+export const getUsers = async (req, res, next) => {
     try {
         const allUsers = await db.select().from(User);
         res.json(allUsers);
     } catch (error) {
         console.log("Database error ", error)
+        next(error)
     }
 }
