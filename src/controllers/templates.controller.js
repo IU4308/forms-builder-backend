15bda698e25651661bd372c27bbca3937e297da9
Ilya <ilya671qw@gmail.com>
2025-04-10 12:31:55 +0300

    add update template action

--- src/controllers/templates.controller.js
+++ src/controllers/templates.controller.js
@@ -4,10 +4,8 @@ import { Template } from "../models/Template.js";
 
 export const createTemplate = async (req, res, next) => {
     const template = req.body;
-    console.log(template)
     try {
         const [insertedTemplate] = await db.insert(Template).values(template).returning({ id: Template.id })
-        console.log(insertedTemplate)
         res.json({ templateId: insertedTemplate.id, message: `The template has been published successfully` });
 
     } catch (error) {
@@ -15,9 +13,21 @@ export const createTemplate = async (req, res, next) => {
     }
 }
 
+export const updateTemplate = async (req, res, next) => {
+    const updatedTemplate = req.body;
+    const { templateId } = req.params; 
+    try {
+        await db.update(Template)
+                .set(updatedTemplate)
+                .where(eq(Template.id, templateId));
+        res.json({ message: 'The template has been updated successfully' })
+    } catch (error) {
+        next (error)
+    }
+}
+
 export const getTemplate = async (req, res, next) => {
     const { templateId } = req.params
-    console.log('get template request')
     try {
         const [template] = await db.select().from(Template).where(eq(Template.id, templateId));
         res.json(template)
--- src/routes/templates.routes.js
+++ src/routes/templates.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createTemplate, getTemplate, getUserTemplates } from "../controllers/templates.controller.js";
+import { createTemplate, getTemplate, getUserTemplates, updateTemplate } from "../controllers/templates.controller.js";
 
 const templatesRouter = Router();
 
 templatesRouter.post('/', createTemplate)
 templatesRouter.get('/:templateId', getTemplate)
+templatesRouter.put('/:templateId', updateTemplate)
 templatesRouter.get('/users/:userId', getUserTemplates)
 
 export default templatesRouter;
