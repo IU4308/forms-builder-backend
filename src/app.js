10ea9a2958b3bfdd32ff46f7443c55a0b1cd7186
Ilya <ilya671qw@gmail.com>
2025-04-22 18:14:13 +0300

    set up soket io server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { createServer } from 'http';
 import config from './config/env.js';
 import authRouter from './routes/auth.routes.js';
 import cors from 'cors';
@@ -9,9 +10,15 @@ import adminRouter from './routes/admin.routes.js';
 import templatesRouter from './routes/templates.routes.js';
 import formsRouter from './routes/forms.routes.js';
 import homeRouter from './routes/home.routes.js';
+import { Server } from 'socket.io';
 
 const app = express();
-
+const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+      origin: '*',
+  }
+});
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
@@ -30,10 +37,15 @@ app.use('/api/admin', adminRouter)
 app.use('/api/templates', templatesRouter)
 app.use('/api/forms', formsRouter)
 
+io.on('connection', async (socket) => {
+  console.log('New client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+})
+
 app.use(errorHandler)
 
-app.listen(config.port, () => {
+server.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
 });
-
-export default app;
