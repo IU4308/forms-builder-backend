17c1ca19b695286fda6630547684d3d35a434cfe
Ilya <ilya671qw@gmail.com>
2025-05-08 14:13:33 +0300

    refactor interaction with salesforce using jsforce

--- src/config/env.js
+++ src/config/env.js
@@ -9,7 +9,11 @@ const config = {
     frontendUrl: process.env.FRONTEND_URL,
     secretKey: process.env.AUTH_SECRET,
     sfAccessToken: process.env.SF_ACCESS_TOKEN,
-    sfInstanceUrl: process.env.SF_INSTANCE_URL,
+    SF_INSTANCE_URL: process.env.SF_INSTANCE_URL,
+    SF_CLIENT_ID: process.env.SF_CLIENT_ID,
+    SF_LOGIN_URL: process.env.SF_LOGIN_URL,
+    SF_USERNAME: process.env.SF_USERNAME,
+    SF_PASSWORD: process.env.SF_PASSWORD,
 };
 
 export default config;
--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { User } from "../models/User.js";
 import { findAll, findOneById } from "../utils/utils.js";
+import { Connection } from 'jsforce';
 import config from "../config/env.js";
-import { getAccesToken } from "../config/salesforce-token.js";
 
 export const getUsers = async (req, res, next) => {
     try {
@@ -24,57 +24,39 @@ export const getUserById = async (req, res, next) => {
 export const createSalesforseAccount = async (req, res, next) => {
     const { name, email, phone, leadSource, consent } = req.body;
     try {
-        const { access_token, instance_url } = await getAccesToken();
+        const conn = new Connection({
+            loginUrl: config.SF_LOGIN_URL
+        });
 
-        const payload = {
-        allOrNone: true,
-        compositeRequest: [
-            {
-                method: 'POST',
-                url: '/services/data/v63.0/sobjects/Account',
-                referenceId: 'NewAccount',
-                body: {
-                    Name: name,
-                    Description: 'Account created from form submission',
+        await conn.login(config.SF_USERNAME, config.SF_PASSWORD);
+
+        const result = await conn.requestPost('/services/data/v63.0/composite', {
+            allOrNone: true,
+            compositeRequest: [
+                {
+                    method: 'POST',
+                    url: '/services/data/v63.0/sobjects/Account',
+                    referenceId: 'NewAccount',
+                    body: { Name: name, Description: 'Created via form' },
                 },
-            },
-            {
-                method: 'POST',
-                url: '/services/data/v63.0/sobjects/Contact',
-                referenceId: 'NewContact',
-                body: {
+                {
+                    method: 'POST',
+                    url: '/services/data/v63.0/sobjects/Contact',
+                    referenceId: 'NewContact',
+                    body: {
                     LastName: name,
                     Email: email,
                     Phone: phone,
                     LeadSource: leadSource,
                     Wants_Newsletters__c: consent === 'on',
                     AccountId: '@{NewAccount.id}',
+                    },
                 },
-            },
-        ],
-        };
-
-        const result = await axios.post(
-            `${instance_url}/services/data/v63.0/composite`,
-            payload,
-            {
-                headers: {
-                    Authorization: `Bearer ${access_token}`,
-                    'Content-Type': 'application/json',
-                },
-            }
-        );
-
-    res.json({
-        result: result.data,
-        message: 'Information has been saved'
-    });
+            ],
+        });
+      
+        res.json({ message: 'Saved successfully', result });
     } catch (error) {
-        if (error.response) {
-          console.error('Salesforce error:', error.response.data);
-        } else {
-          console.error('Unexpected error:', error.message);
-        }
-        throw error;
-      }
+        next(error)
+    }
 }
